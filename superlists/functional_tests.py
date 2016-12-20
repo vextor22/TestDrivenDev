bd80d9d340b5aea2a36016cd5b3668393aa497ba
Matt Higgins <mrhiggi@g.clemson.edu>
2016-12-20 01:28:30 -0500

    Functional test now tries to add element to list

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -17,10 +18,28 @@ class NewVisitorTest(unittest.TestCase):
 				self.browser.get('http://127.0.0.1:8000')
 
 				self.assertIn('To-Do', self.browser.title)
-				self.fail('Finish the test!')
 
-#add an item to the list
-#add another item to the list
-#navigate to unique url to verify list persists
+				header_text = self.browser.find_element_by_tag_name('h1').text
+				self.assertIn('To-Do', header_text)
+				#user is prompted for item
+				inputbox = self.browser.find_element_by_id('id_new_item')
+				self.assertEqual(
+								inputbox.get_attribute('placeholder'),
+								'Enter a to-do item'
+								)
+
+				#add an item to the list
+				inputbox.send_keys('Buy peacock feathers')
+				
+				inputbox.send_keys(Keys.ENTER)
+				#page is updated with the item
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertTrue(
+								any(row.text == '1: Buy peacock feathers' for row in rows)
+								)
+				self.fail('Finish the test!')
+				#add another item to the list
+				#navigate to unique url to verify list persists
 if __name__ == '__main__':
 		unittest.main(warnings='ignore')

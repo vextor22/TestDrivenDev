a9b3366aae05622abce7aae895bf81322cff42d7
Matt Higgins <mrhiggi@g.clemson.edu>
2016-12-20 17:19:26 -0500

    List app handles 1 list item, but not multiple

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-
+import time
 
 #looks at page title
 class NewVisitorTest(unittest.TestCase):
@@ -30,16 +30,21 @@ class NewVisitorTest(unittest.TestCase):
 
 				#add an item to the list
 				inputbox.send_keys('Buy peacock feathers')
-				
+				inputbox.send_keys(Keys.ENTER)
+
+				inputbox = self.browser.find_element_by_id('id_new_item')
+
+				inputbox.send_keys('Use peacock feathers to make a fly')
 				inputbox.send_keys(Keys.ENTER)
 				#page is updated with the item
 				table = self.browser.find_element_by_id('id_list_table')
 				rows = table.find_elements_by_tag_name('tr')
-				self.assertTrue(
-							any(row.text == '1: Buy peacock feathers' for row in rows),
-							"New to-do item did not appear in table"
-								)
+				self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+				self.assertIn('2: Use peacock feathers to make a fly',
+								[row.text for row in rows])
+
 				self.fail('Finish the test!')
+
 				#add another item to the list
 				#navigate to unique url to verify list persists
 if __name__ == '__main__':

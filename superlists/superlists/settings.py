8e02172bcc857717b73b7774f455f32fccfaf053
Matt Higgins <mrhiggi@g.clemson.edu>
2016-12-19 22:20:29 -0500

    rearranged files

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7%&y5@nji3#=y_cnxi-u7b^a*_(=y3olj9uw7em@ecwj=&*fm@'
+SECRET_KEY = '3r@a54hlj5%7c9z%)14^(_-deob%+a4_)=wcpu0r=ko&0y!c_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

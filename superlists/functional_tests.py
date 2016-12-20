3dd6adbf216c409d49b44a5789b141f0caf58090
Matt Higgins <mrhiggi@g.clemson.edu>
2016-12-19 23:43:55 -0500

    Added simple comments

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -8,6 +8,7 @@ class NewVisitorTest(unittest.TestCase):
 		
 		def setUp(self):
 				self.browser = webdriver.Firefox()
+				self.browser.implicitly_wait(3)
 
 		def tearDown(self):
 				self.browser.quit()
@@ -18,5 +19,8 @@ class NewVisitorTest(unittest.TestCase):
 				self.assertIn('To-Do', self.browser.title)
 				self.fail('Finish the test!')
 
+#add an item to the list
+#add another item to the list
+#navigate to unique url to verify list persists
 if __name__ == '__main__':
 		unittest.main(warnings='ignore')

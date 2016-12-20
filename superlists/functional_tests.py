06cd5a725e673c4e6044fef3c1934451296047d4
Matt Higgins <mrhiggi@g.clemson.edu>
2016-12-19 22:37:02 -0500

    developed user story for ft

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,26 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+#user navigates to our home page
 browser.get('http://127.0.0.1:8000')
-assert 'Selenium' in browser.title
+
+#looks at page title
+assert 'To-Do' in browser.title
+
+#Enters a todo item straight away (Buy peacock feathers)
+
+#Hits enter, page updates and lists
+#1: Buy peacock feathers" as item in to-do list
+
+#still a box to add another item
+#enters (Use peacock feathers to make a fly)
+
+#page updates, both items in list
+
+#wonders whether site remembers the list, visits the unique url of list,
+#list is still there
+
+#satisfied, user leaves
+
+browser.quit()

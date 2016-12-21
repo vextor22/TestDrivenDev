4b6926e4640ed830c2be19b32205904435cc2501
Matt Higgins <mrhiggi@g.clemson.edu>
2016-12-20 19:11:41 -0500

    redirect after post, now displaying items from db

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -36,12 +36,12 @@ class NewVisitorTest(unittest.TestCase):
 				#add an item to the list
 				inputbox.send_keys('Buy peacock feathers')
 				inputbox.send_keys(Keys.ENTER)
-
+				time.sleep(2)
 				inputbox = self.browser.find_element_by_id('id_new_item')
 
 				inputbox.send_keys('Use peacock feathers to make a fly')
 				inputbox.send_keys(Keys.ENTER)
-
+				time.sleep(1)
 				#page is updated with the item
 				self.check_for_row_in_list_table('1: Buy peacock feathers')
 				self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
